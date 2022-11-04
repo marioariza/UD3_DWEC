0476a05d9063321a38609e86e62c87cb3bf05d02
*Mario <*mariojra6@gmail.com*>
2022-11-04 12:18:10 +0100

    Archivos actualizados

--- ACT3/003minimize.js
+++ ACT3/003minimize.js
@@ -1,10 +1,11 @@
 "use strict";
 
-let a = prompt('Introduzca el primer número:');
-let b = prompt('Introduzca el segundo número:');
+let a = (parseInt(prompt('Introduzca el primer número:'))); // Convertimos a int el dato que recogemos para que no de problemas en nuestra función.
+// Un ejemplo de fallo que podría dar es que el número 12345 sea menor que 3, esto se debe a que no se recoge como int, pero con esta función lo arreglamos.
+let b = (parseInt(prompt('Introduzca el segundo número:')));
 
 alert('El menor entre ' + a + ' y ' + b +' es: ' + min(a,b));
 
 function min(a,b) {
-    return Math.min(a,b); // Con el método Math.main() calculamos al instante el número menor entre los dos números introducidos.
-}
+    return Math.min(a,b); // Con el método Math.min() calculamos al instante el número menor entre los dos números introducidos.
+} 
